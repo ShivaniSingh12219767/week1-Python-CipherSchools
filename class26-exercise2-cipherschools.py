15f8734e39b904d3ca5538e5771e13cc130cd638
ShivaniSIngh <shivani.singh12219767@gmail.com>
2022-12-01 21:57:02 +0530

    commit

--- class26-exercise2-cipherschools.py
+++ class26-exercise2-cipherschools.py
@@ -1,6 +1,8 @@
 # Ask user to input 3 numbers and you have to print average of three numbers using string formatting
 # Take input in one line
-a,b,c=int(input("Enter 1st,2nd,3rd number: ")).split()
-
+a,b,c=input("Enter 1st,2nd,3rd number: ").split()
+a=int(a)
+b=int(b)
+c=int(c)
 average=(a+b+c)/3
 print(f"Average of {a},{b},{c},is:",average)
